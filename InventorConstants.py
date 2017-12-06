d1f4aea929ffe40312af8d930816ce615a7051ae
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-12-06 10:11:10 +0100

    Adding some constants

---
+++ InventorConstants.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2017 Thomas Karl Pietrowski
+
+class ExportUnits:
+    Inch = 2
+    Foot = 3
+    Centimeter = 4
+    Millimeter = 5
+    Meter = 6
+    Micron = 7
+
+class Resolution:
+    High = 0
+    Medium = 1
+    Low = 2
+    Custom = 3
+    
+class OutputFileType:
+    binary = 0
+    ASCII = 1
+    
